c6375f8e5b35a17d76dc271afcd287e0cf30f7b0
kaiharvest <indradwiprabowo27@gmail.com>
2025-10-27 22:13:20 +0700

    Thread Communication

--- src/test/java/id/indradwiprabowo/threaddemo/ThreadCommunicationTest.java
+++ src/test/java/id/indradwiprabowo/threaddemo/ThreadCommunicationTest.java
@@ -6,6 +6,7 @@ public class ThreadCommunicationTest {
 
     private String message = null;
 
+    // sangat tidak dianjurkan karena bisa membuang-buang cpu
     @Test
     void thread() throws InterruptedException {
         var thread1 = new Thread(() -> {
@@ -25,4 +26,77 @@ public class ThreadCommunicationTest {
         thread2.join();
         thread1.join();
     }
+
+    // disarankan menggunakan ini
+    @Test
+    void waitNotify() throws InterruptedException {
+        final var lock = new Object();
+
+        var thread1 = new Thread(() -> {
+            synchronized (lock) {
+                try {
+                    lock.wait();
+                    System.out.println(message);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        var thread2 = new Thread(() -> {
+            synchronized (lock) {
+                message = "Indra Dwi Prabowo";
+                lock.notify();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+    }
+
+    @Test
+    void waitNotifyAll() throws InterruptedException {
+        final var lock = new Object();
+
+        var thread1 = new Thread(() -> {
+            synchronized (lock) {
+                try {
+                    lock.wait();
+                    System.out.println(message);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        var thread3 = new Thread(() -> {
+            synchronized (lock) {
+                try {
+                    lock.wait();
+                    System.out.println(message);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        var thread2 = new Thread(() -> {
+            synchronized (lock) {
+                message = "Indra Dwi Prabowo";
+                lock.notifyAll();
+            }
+        });
+
+        thread1.start();
+        thread3.start();
+        thread2.start();
+
+        thread1.join();
+        thread3.join();
+        thread2.join();
+    }
+
 }
