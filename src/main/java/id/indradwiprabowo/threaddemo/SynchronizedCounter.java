e82b54813e3615291a33cef8c5d130b15ffd978e
kaiharvest <indradwiprabowo27@gmail.com>
2025-10-27 21:28:09 +0700

    Synchronized

---
+++ src/main/java/id/indradwiprabowo/threaddemo/SynchronizedCounter.java
@@ -0,0 +1,17 @@
+package id.indradwiprabowo.threaddemo;
+
+public class SynchronizedCounter {
+
+    private Long value = 0L;
+
+    public void increment() {
+        synchronized (this) {
+            value++;
+        }
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+}
---
+++ src/test/java/id/indradwiprabowo/threaddemo/SynchronizedTest.java
@@ -0,0 +1,33 @@
+package id.indradwiprabowo.threaddemo;
+
+import org.junit.jupiter.api.Test;
+
+public class SynchronizedTest {
+
+    @Test
+    void counterTest() throws InterruptedException {
+
+        var counter = new SynchronizedCounter();
+        Runnable runnable = () -> {
+            for (int i = 0; i < 1_000_000; i++) {
+                counter.increment();
+            }
+        };
+
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
+        Thread thread3 = new Thread(runnable);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        thread1.join();
+        thread2.join();
+        thread3.join();
+
+        System.out.println(counter.getValue());
+
+    }
+
+}
