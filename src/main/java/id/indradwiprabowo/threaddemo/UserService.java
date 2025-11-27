d175ff5f3a89b2c971a73d0f4e493e95453a184d
kaiharvest <indradwiprabowo27@gmail.com>
2025-11-27 10:32:17 +0700

    Thread Local and Local Random

---
+++ src/main/java/id/indradwiprabowo/threaddemo/UserService.java
@@ -0,0 +1,19 @@
+package id.indradwiprabowo.threaddemo;
+
+public class UserService {
+
+    // pakai thread local
+    final ThreadLocal<String> threadLocal = new ThreadLocal<>();
+    //private String user;
+
+    public void setUser(String user) {
+        threadLocal.set(user);
+        // this.user = user;
+    }
+
+    public void doAction() {
+        var user = threadLocal.get();
+        System.out.println(user + " do action");
+    }
+
+}
